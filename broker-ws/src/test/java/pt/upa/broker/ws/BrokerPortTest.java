670443a0bde3d48b7070c3bb6c543be870278f03
drlferreira <diogo.lopes.ferreira@tecnico.ulisboa.pt>
2016-04-14 12:25:00 +0100

    example for mocks on broker

--- broker-ws/src/test/java/pt/upa/broker/ws/BrokerPortTest.java
+++ broker-ws/src/test/java/pt/upa/broker/ws/BrokerPortTest.java
@@ -1,8 +1,11 @@
 package pt.upa.broker.ws;
 
 
+import com.sun.corba.se.pept.broker.Broker;
+import mockit.Expectations;
 import mockit.Mocked;
 import mockit.NonStrictExpectations;
+import mockit.Verifications;
 import org.junit.*;
 import pt.upa.transporter.ws.BadLocationFault_Exception;
 import pt.upa.transporter.ws.BadPriceFault_Exception;
@@ -11,18 +14,22 @@ import pt.upa.transporter.ws.JobView;
 import pt.upa.transporter.ws.cli.TransporterClient;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeMap;
 
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertTrue;
 
 
 public class BrokerPortTest {
 
 	// static members
     @Mocked TransporterClient _tca;
+    private String _upa1 = "UpaTransporter1";
+    private String _upa2 = "UpaTransporter2";
 
 	// one-time initialization and clean-up
-
 
     @BeforeClass
     public static void oneTimeSetUp() {
@@ -32,13 +39,7 @@ public class BrokerPortTest {
     public static void oneTimeTearDown() {
     }
 
-
-	/*String[][] regions = {{"Porto", "Braga", "Viana do Castelo", "Vila Real", "Bragança"},
-			{"Lisboa","Leiria", "Santarém", "Castelo Branco", "Coimbra", "Aveiro", "Viseu", "Guarda"},
-			{"Setúbal", "Évora", "Portalegre", "Beja", "Faro"}};
-	*/
 	// members
-
 
     // initialization and clean-up for each test
 
@@ -49,7 +50,62 @@ public class BrokerPortTest {
 
    @After
     public void tearDown() {
-    	
+    }
+
+    
+    @Test
+    public void simpleAccept() throws Exception {
+        // Preparation code not specific to JMockit, if any.
+        String origin = "Lisboa";
+        String destination = "Porto";
+        String id = "0";
+        int price = 0;
+        JobView jv = createJobView(JobStateView.PROPOSED, price, origin, destination, id, _upa2);
+
+
+        // METES O QUE ESPERAS QUE O TRANSPORTER CLIENT RETORNE AO BROKER OU SEJA QUANDO O BROKER FOR CHAMAR O TRANSPORTER CLIENT O QUE VAI RECEBER PARA OPERAR SOBRE ESSES DADOS
+        // an "expectation block"
+        // One or more invocations to mocked types, causing expectations to be recorded.
+        new Expectations() {{
+            _tca.requestJob("Lisboa", "Porto", 0);
+            result = jv;
+            _tca.decideJob(id, true);
+            jv.setJobState(JobStateView.ACCEPTED);
+            _tca.jobStatus(id);
+            result = jv;
+        }};
+
+        // EXECUTAS OS METODOS DO BROKER NORMALMENTE
+        // Unit under test is exercised.
+        BrokerPort bp = new BrokerPort(_tca);
+
+
+        String idOfRequestedJob = bp.requestTransport(origin, destination, 0);
+        TransportView outputTv = bp.viewTransport(id);
+
+        // O NUMERO DE VEZES QUE PODE SER CHAMADA CADA FUNCAO DO TRANSPORTER CLIENT A PARTIR DO BROKER COM X ARGUMENTOS
+        // One or more invocations to mocked types, causing expectations to be verified.
+        new Verifications() {{
+            // Verifies that zero or one invocations occurred, with the specified argument value:
+            _tca.requestJob(origin, destination, price); maxTimes = 1;
+            _tca.decideJob(id, true); maxTimes = 1;
+            _tca.jobStatus(id); maxTimes = 1;
+        }};
+
+        // FAZES OS TESTES AQUI DOS ASSERTS
+        // Additional verification code, if any, either here or before the verification block.
+        assertEquals("The Correct ID was not returned.", id, idOfRequestedJob);
+
+        List<TransportView> tvs = bp.listTransports();
+        assertEquals(1,tvs.size());
+
+        for(TransportView tv : tvs){
+            if(tv.getId().equals(id)){
+                assertTrue(comparateTransportsViews(outputTv,tv));
+                assertEquals(TransportStateView.BOOKED, tv.getState());
+            }
+        }
+
     }
 
 
@@ -227,6 +283,29 @@ public List<TransportView> listTransports() {
         //Mock a couple transporters, then ask to clear data???
         _bp.clearTransports();
     }
+
+
 */
+
+    /* Aux functions */
+
+    private JobView createJobView(JobStateView state, int price, String destination, String origin, String id, String companyName){
+        JobView jb = new JobView();
+        jb.setJobState(state);
+        jb.setJobPrice(price);
+        jb.setJobDestination(destination);
+        jb.setJobOrigin(origin);
+        jb.setJobIdentifier(id);
+        jb.setCompanyName(companyName);
+        return jb;
+    }
+
+
+    private boolean comparateTransportsViews(TransportView tv1, TransportView tv2){
+        return tv1.getState() == tv2.getState() && tv1.getPrice() == tv2.getPrice() && tv1.getDestination().equals(tv2.getDestination()) &&
+                tv1.getOrigin().equals(tv2.getOrigin()) && tv1.getTransporterCompany().equals(tv2.getTransporterCompany());
+    }
+
+
 
 }
