d64c4715af254a1ec65b1aceed650e1f8a22d9bc
drlferreira <diogo.lopes.ferreira@tecnico.ulisboa.pt>
2016-05-02 12:25:47 +0100

    Added folder structure

---
+++ ca-ws-cli/src/main/java/pt/upa/ca/CaClientApplication.java
@@ -0,0 +1,9 @@
+package pt.upa.ca;
+
+public class CaClientApplication {
+
+	public static void main(String[] args) throws Exception {
+	
+	}
+	
+}
---
+++ ca-ws/src/main/java/pt/upa/ca/CaApplication.java
@@ -0,0 +1,9 @@
+package pt.upa.ca;
+
+public class CaApplication {
+
+	public static void main(String[] args) throws Exception {
+	
+	}
+	
+}
