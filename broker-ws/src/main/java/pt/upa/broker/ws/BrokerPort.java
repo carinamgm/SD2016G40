6fa2d60c0b881bcb207e2ef974d3a96946a00c9a
carinamgm <carinamartins@outlook.com>
2016-05-09 16:58:56 +0100

    passo 2: update

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -6,12 +6,16 @@ import pt.upa.transporter.ws.BadPriceFault_Exception;
 import pt.upa.transporter.ws.JobView;
 import pt.upa.transporter.ws.cli.TransporterClient;
 
-import javax.jws.HandlerChain;
 import javax.jws.WebService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import javax.xml.ws.BindingProvider;
+import java.util.Map;
+
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
 
 @WebService(
         endpointInterface="pt.upa.broker.ws.BrokerPortType",
@@ -26,12 +30,26 @@ public class BrokerPort implements BrokerPortType {
     private List<String> _north = Arrays.asList("Porto", "Braga", "Viana do Castelo", "Vila Real", "Bragança");
     private List<String> _south = Arrays.asList("Setúbal", "Évora", "Portalegre", "Beja", "Faro");
 	private List<TransportView> _tvs = Collections.synchronizedList(new ArrayList<TransportView>());
+	private List<TransportView> _auxUpdate = new ArrayList<TransportView>();
     private TransporterClient _tca;
-
+    private BrokerPortType _sb = null;
+    
     public BrokerPort(){}
 
     public BrokerPort(TransporterClient tca){
+    	_tca = tca;
+    }
+
+    public BrokerPort(TransporterClient tca, String url){
         _tca = tca;
+        if(!url.equals("")){
+    	  BrokerService service = new BrokerService();
+          _sb = service.getBrokerPort();
+
+          BindingProvider bindingProvider = (BindingProvider) _sb;
+          Map<String, Object> requestContext = bindingProvider.getRequestContext();
+          requestContext.put(ENDPOINT_ADDRESS_PROPERTY, url);
+        }
     }
 
     @Override
@@ -105,6 +123,10 @@ public class BrokerPort implements BrokerPortType {
         }
 
         _tvs.add(tv);
+        if(_sb == null){
+        	_auxUpdate.add(tv);
+        	updateSecondaryBroker("request", _auxUpdate);
+        }
         return tv.getId();
     }
 
@@ -154,6 +176,10 @@ public class BrokerPort implements BrokerPortType {
                             tv.setState(TransportStateView.COMPLETED);
                             break;
                     }
+                    if(_sb == null){
+                    	_auxUpdate.add(tv);
+                    	updateSecondaryBroker("view", _auxUpdate);
+                    }
                     return tv;
                 }
                 throw new UnknownTransportFault_Exception(id, utf);
@@ -171,13 +197,29 @@ public class BrokerPort implements BrokerPortType {
     public void clearTransports(){
     	_tvs.clear();
     	_tca.clearTransports();
-    	updateSecondaryBroker();
+    	if(_sb == null)
+    		updateSecondaryBroker("clear", _tvs);
     }
 
 	@Override
-	public void updateSecondaryBroker() {
-		// TODO Auto-generated method stub
-		
+	public void updateSecondaryBroker(String func, List<TransportView> auxUpdate) {
+		if(_sb != null){
+			if(func.equals("clear"))
+				_sb.clearTransports();
+			else{
+				stateAlreadyInTVS(auxUpdate);
+				_tvs.addAll(auxUpdate);	
+			}
+		}	
 	}
-
+	
+	private void stateAlreadyInTVS(List<TransportView> auxUpdate) {
+		if(_sb != null){
+			for(int i = 0; i < auxUpdate.size();i++){
+				if(_tvs.contains(auxUpdate.get(i)))
+					_tvs.remove(auxUpdate.get(i));
+			}	
+		}
+	}
+	
 }
